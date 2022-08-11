a2b182af314d558095accc7db42f82092facbc81
Roberto Andres Condezo Monge <ra.condezomonge@gmail.com>
2022-08-11 01:09:08 -0500

    Feat[db/]: [Create migration, schema and seeds files]

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Greeting.create(greeting: 'Good morning!' )
+Greeting.create(greeting: 'Hello!' )
+Greeting.create(greeting: 'Hi!' )
+Greeting.create(greeting: 'Hi!' )
+Greeting.create(greeting: 'Good afternoon!' )
